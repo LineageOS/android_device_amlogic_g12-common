20584bc7666f91e4f9c2c4f670a89438ff3b8160
Nolen Johnson <johnsonnolen@gmail.com>
2021-10-30 23:35:24 -0400

    g12: Comply with property isolation
    * Stock has isolation enabled, and yet has these all in
      /vendor/build.prop? - eh, we can do it correctly.
    
    Change-Id: I547277eae2f0eadf873670bc79778f3401f036ba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,6 +105,7 @@ TARGET_BOARD_PLATFORM := amlogic
 
 ## Properties
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 ## Soong Namespace
