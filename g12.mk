521b1e146f03a25eb773cfd1c8465754c8819d88
Nolen Johnson <johnsonnolen@gmail.com>
2022-05-02 11:45:30 -0400

    g12: Swap to hardware OMX codecs
    * These FINALLY work.
    * This reverts 4619ddf8762c15e9c6c116934cfea2dc6cd90706 partially,
      as the media_codecs.xml file was updated in 12 import.
    
    Change-Id: I52949e71865268f04c92a2d5bb42ede7d03f9396

--- g12.mk
+++ g12.mk
@@ -68,7 +68,7 @@ PRODUCT_IS_ATV := true
 
 ## Codecs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs_sw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
