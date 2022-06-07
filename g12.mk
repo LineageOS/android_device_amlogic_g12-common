69dccc7c516aef83f3751ca968ec44d272f940e0
Nolen Johnson <johnsonnolen@gmail.com>
2022-06-07 17:23:12 -0400

    g12: Move common stuff to amlogic/common
    Change-Id: I45b8d767aa27dcafc0c19fb4e5af814ff40bd2eb

--- g12.mk
+++ g12.mk
@@ -15,54 +15,24 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hearing_aid_audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/msd_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/msd_audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/surround_sound_configuration_5_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/surround_sound_configuration_5_0.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@4.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@7.0.vendor \
     android.hardware.audio@7.0-util.vendor \
     android.hardware.audio.common@7.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@7.0-util.vendor \
-    android.hardware.audio.service \
-    audio.a2dp.default \
-    audio.r_submix.default \
-    audio.usb.default \
     av-types-aidl-cpp.vendor \
     libaudioclient_aidl_conversion_util.vendor \
     libaudiofoundation.vendor
 
 ## Bluetooth
-ifeq ($(BOARD_HAVE_BLUETOOTH),false)
-PRODUCT_PROPERTY_OVERRIDES += \
-    config.disable_bluetooth=true
-else
+ifneq ($(BOARD_HAVE_BLUETOOTH),false)
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.autoconnectbt.btclass=50c \
-    ro.vendor.autoconnectbt.isneed=false \
-    ro.vendor.autoconnectbt.macprefix=00:CD:FF \
-    ro.vendor.autoconnectbt.nameprefix=Amlogic_RC \
-    ro.vendor.autoconnectbt.rssilimit=70
-
-PRODUCT_COPY_FILES +=  \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
+    android.hardware.bluetooth@1.0.vendor
 endif
-
-## Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
 
 ## Camera
 PRODUCT_PACKAGES += \
@@ -80,51 +50,19 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
-ifeq ($(PRODUCT_USE_SW_OMX),true)
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs_sw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
-else
+ifneq ($(PRODUCT_USE_SW_OMX),true)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 endif
 
-## DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
-    libdrm.vendor
-
 ## dumpstate
 PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.1.vendor
 
-## fastbootd
-PRODUCT_PACKAGES += fastbootd
-
-## File-system permissions
-PRODUCT_PACKAGES += \
-    fs_config_dirs \
-    fs_config_files
-
-## Gatekeeper
-PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-service.software
-
 ## Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4.vendor
-
-## Hardware Compsoer
-PRODUCT_PACKAGES += \
-    libhwc2on1adapter \
-    libhwc2onfbadapter
-
-## HDMI CEC
-PRODUCT_PACKAGES += \
-    android.hardware.tv.cec@1.0-impl \
-    android.hardware.tv.cec@1.0-service
 
 ## Health
 PRODUCT_PACKAGES += \
@@ -139,12 +77,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.usb.rc \
     $(LOCAL_PATH)/init-files/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc \
     $(LOCAL_PATH)/init-files/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
-
-## Kernel Modules
-PRODUCT_PACKAGES += \
-    mali \
-    media \
-    optee-module
 
 ## Keymaster
 PRODUCT_PACKAGES += \
@@ -164,21 +96,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 endif
 
-## Light
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
-
-## Memtrack
-PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
-
-## Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 ## Partitions
 BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT  := true
 BUILDING_SUPER_EMPTY_IMAGE := true
@@ -187,26 +104,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 ## Permissions
 PRODUCT_COPY_FILES +=  \
-    frameworks/native/data/etc/android.hardware.audio.output.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.output.xml \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.gamepad.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.gamepad.xml \
-    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hdmi.cec.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.software.app_widgets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.app_widgets.xml \
-    frameworks/native/data/etc/android.software.backup.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.backup.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
-    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
-    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
 ## Platform
@@ -223,23 +127,13 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
-## Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 ## USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
     android.hardware.usb.gadget@1.2.vendor
 
 ## Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
-    hostapd \
-    libwpa_client \
-    WifiOverlay \
-    wpa_supplicant \
-    wpa_supplicant.conf
+    android.hardware.wifi@1.0-service
 
 ## Inherit from the main common tree product makefile
 $(call inherit-product, device/amlogic/common/amlogic.mk)
