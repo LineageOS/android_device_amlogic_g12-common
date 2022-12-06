5b776847ca2d00f543731602c05ccc25084ba65d
Nolen Johnson <johnsonnolen@gmail.com>
2022-12-06 01:25:27 -0500

    aml: Build necessary ndk_platform shims needed by blobs
    * Really not sure how this works on stock, libs just aren't there.
    
    Change-Id: I9720bc74b1f0a5d441fec6ba2569b460ec1b2b79

--- g12.mk
+++ g12.mk
@@ -81,6 +81,9 @@ PRODUCT_PACKAGES += \
 
 ## Keymaster
 PRODUCT_PACKAGES += \
+    android.hardware.security.keymint-V1-ndk_platform.vendor \
+    android.hardware.security.secureclock-V1-ndk_platform.vendor \
+    android.hardware.security.sharedsecret-V1-ndk_platform.vendor \
     lib_android_keymaster_keymint_utils.vendor \
     libkeymint.vendor \
     libkeymaster_messages.vendor \
@@ -95,6 +98,10 @@ ifeq ($(TARGET_HAS_TEE),false)
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 endif
+
+## OEM Lock
+PRODUCT_PACKAGES += \
+    android.hardware.oemlock@1.0.vendor
 
 ## Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -114,6 +121,10 @@ PRODUCT_COPY_FILES +=  \
 
 ## Platform
 TARGET_AMLOGIC_SOC ?= g12a
+
+## Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-V2-ndk_platform.vendor
 
 ## Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
