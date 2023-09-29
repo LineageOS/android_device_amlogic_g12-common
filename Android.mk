4a426f7e7a7c656a3e31cec97cf3d270f3b02a41
Bruno Martins <bgcngm@gmail.com>
2023-09-29 17:11:24 +0100

    g12-common: Fix Vulkan support
    Quite funny, the lib is actually a symlink to ../egl/libGLES_mali.so.
    
    Change-Id: Ib185c9acb14e15fa358becd3973afeb0a4e8ad28

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,5 +8,15 @@ ifneq ($(filter g12a g12b sm1,$(TARGET_AMLOGIC_SOC)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+VULKAN_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw
+$(VULKAN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Vulkan lib link: $@"
+	@mkdir -p $@
+	$(hide) ln -sf /vendor/lib/egl/libGLES_mali.so $@/vulkan.amlogic.so.so
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINK)
 
 endif
