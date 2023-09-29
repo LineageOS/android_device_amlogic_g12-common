7698173473bfee37582a47370008249062839edd
Bruno Martins <bgcngm@gmail.com>
2023-09-29 14:50:10 +0100

    g12-common: Skip ATV specific blobs on non-ATV targets
    Change-Id: Ifaec95a99ce028dba5be2547919ff35b93542048

--- extract-files.sh
+++ extract-files.sh
@@ -75,6 +75,7 @@ if [ -z "${ONLY_TARGET}" ]; then
     setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files-atv.txt" "${SRC}" "${KANG}" --section "${SECTION}"
     extract "${MY_DIR}/proprietary-files-tee.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
