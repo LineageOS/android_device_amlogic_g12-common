4f4974cd2677bcfa0ff04aaed79b7d70b25aeb2c
Bruno Martins <bgcngm@gmail.com>
2023-10-31 12:53:22 +0000

    g12-common: Run systemcontrol_static in recovery mode
    This is done to ensure that proper display resolution is set while
    device is in recovery mode. Meson config file is required, so import
    it into the device tree in order to avoid uncommon copy rules.
    
    Change-Id: I3a14e02f8b857a8007a34fbbe902949ead3373a2

--- g12.mk
+++ g12.mk
@@ -75,6 +75,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/mesondisplay.cfg:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/mesondisplay.cfg \
+    $(LOCAL_PATH)/configs/mesondisplay.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mesondisplay.cfg
+
 ## Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor
