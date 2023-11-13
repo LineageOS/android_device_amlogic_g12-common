fa91916f9f5b6cb6dfd1cda76fb3897a30e05358
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-12 23:12:02 -0500

    g12-common: Copy ramdisk to first_stage_ramdisk as well
    * Fixes up 1acf46eebcc8452303c55d9c344e254edcf5542e which dropped this.
    
    Change-Id: I365f7af37b5c9830e95650eeeb55027e70162c22

--- init-files/Android.mk
+++ init-files/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES        := fstab.amlogic
 else
 LOCAL_SRC_FILES        := fstab_no_avb.amlogic
 endif
-LOCAL_REQUIRED_MODULES := fstab.amlogic_ramdisk
+LOCAL_REQUIRED_MODULES := fstab.amlogic_ramdisk fstab.amlogic_first_stage_ramdisk
 LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -24,4 +24,17 @@ else
 LOCAL_SRC_FILES        := fstab_no_avb.amlogic
 endif
 LOCAL_MODULE_PATH      := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE           := fstab.amlogic_first_stage_ramdisk
+LOCAL_MODULE_STEM      := fstab.amlogic
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_CLASS     := ETC
+ifneq ($(TARGET_HAS_TEE),false)
+LOCAL_SRC_FILES        := fstab.amlogic
+else
+LOCAL_SRC_FILES        := fstab_no_avb.amlogic
+endif
+LOCAL_MODULE_PATH      := $(TARGET_RAMDISK_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
