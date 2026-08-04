4792dbbca73caf9df1456c248698f58c70eb5549
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-04 00:53:50 -0400

    aml: Decommonize bluetooth manifest
    Change-Id: I47c44aca680e4cf540d450abf7cad4b7e674e585

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,6 +15,10 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 ## HIDL
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
+ifneq ($(BOARD_HAVE_BLUETOOTH),false)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_bt.xml
+endif
+
 ## Kernel
 BOARD_KERNEL_CMDLINE := androidboot.dynamic_partitions=true use_uvm=1
 ifeq ($(TARGET_BOOTDEVICE),usb)
