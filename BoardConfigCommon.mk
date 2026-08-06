fb2e222a30a5edeb116496836427ad2755ce2a62
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-08-06 11:36:20 +0800

    g12: Revise partition layout
    This adds system_dlkm, matching sabrina's layout.
    Additionally swaps treble partitions to erofs to play along
    with what is defined in device/amlogic/common tree.
    
    Change-Id: I0c1d75c0f5ac690a655535ba992d71b4a1521baa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ TARGET_KERNEL_VERSION ?= 5.15
 
 ## Partitions
 SSI_PARTITIONS := product system system_ext
-TREBLE_PARTITIONS := odm vendor vendor_dlkm
+TREBLE_PARTITIONS := odm system_dlkm vendor vendor_dlkm
 ALL_PARTITIONS := $(SSI_PARTITIONS) $(TREBLE_PARTITIONS)
 
 BOARD_AMLOGIC_DYNAMIC_PARTITIONS_PARTITION_LIST := $(ALL_PARTITIONS)
