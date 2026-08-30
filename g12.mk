9f7769157fd7749476ba22560f856180a03c75cc
Bruno Martins <bgcngm@gmail.com>
2026-08-30 02:57:11 +0100

    g12: Switch to default task_profiles from AOSP
    Change-Id: Ic19b2ec445fe8bba2750407e6abf3829501d83ff

--- g12.mk
+++ g12.mk
@@ -36,7 +36,7 @@ PRODUCT_PACKAGES += \
 ## Cgroups
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 ## Codecs
 PRODUCT_COPY_FILES += \
