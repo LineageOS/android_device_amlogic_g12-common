8a8f2450c75c90faa68119810cd329c951ade3aa
Bruno Martins <bgcngm@gmail.com>
2026-08-30 01:36:16 +0100

    g12: releasetools: Rewrite image handling
    Change-Id: Ia281a374bf8ddd78554543eb6627b8cb6991eba3

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -17,22 +17,9 @@ def IncrementalOTA_InstallEnd(info):
     return
 
 
-def AddImage(info, folder, basename, dest):
-    name = basename
+def AddImage(info, folder, basename):
     data = info.input_zip.read(folder + basename)
-    common.ZipWriteStr(info.output_zip, name, data)
-    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
-
-
-def AddDtbImage(info, folder, basename):
-    name = basename
-    data = info.input_zip.read(folder + basename)
-    common.ZipWriteStr(info.output_zip, name, data)
-    info.script.AppendExtra('package_extract_file("%s", "/tmp/dtb.img");' %
-                            name)
-    info.script.AppendExtra(
-        'run_program("/system/bin/dd", "if=/tmp/dtb.img", "of=/dev/dtb", "bs=1k", "count=256");'
-    )
+    common.ZipWriteStr(info.output_zip, basename, data)
 
 
 def PrintInfo(info, dest):
@@ -40,19 +27,31 @@ def PrintInfo(info, dest):
         dest.split('/')[-1]))
 
 
+def WriteDtbImage(info, folder, basename):
+    AddImage(info, folder, basename)
+    PrintInfo(info, "/dev/dtb")
+    info.script.AppendExtra('package_extract_file("%s", "/tmp/dtb.img");' %
+                            basename)
+    info.script.AppendExtra(
+        'run_program("/system/bin/dd", "if=/tmp/dtb.img", "of=/dev/dtb", "bs=1k", "count=256");'
+    )
+
+
+def WriteImage(info, folder, basename, dest):
+    AddImage(info, folder, basename)
+    PrintInfo(info, dest)
+    info.script.AppendExtra('package_extract_file("%s", "%s");' %
+                            (basename, dest))
+
+
 def OTA_InstallEnd(info):
-    PrintInfo(info, "/dev/block/by-name/dtbo")
-    AddImage(info, "IMAGES/", "dtbo.img", "/dev/block/by-name/dtbo")
-    PrintInfo(info, "/dev/block/by-name/vbmeta")
-    AddImage(info, "IMAGES/", "vbmeta.img", "/dev/block/by-name/vbmeta")
+    WriteImage(info, "IMAGES/", "dtbo.img", "/dev/block/by-name/dtbo")
+    WriteImage(info, "IMAGES/", "vbmeta.img", "/dev/block/by-name/vbmeta")
     if 'RADIO/dtb.img' in info.input_zip.namelist():
-        PrintInfo(info, "/dev/dtb")
-        AddDtbImage(info, "RADIO/", "dtb.img")
+        WriteDtbImage(info, "RADIO/", "dtb.img")
     if 'RADIO/logo.img' in info.input_zip.namelist():
-        PrintInfo(info, "/dev/block/by-name/logo")
-        AddImage(info, "RADIO/", "logo.img", "/dev/block/by-name/logo")
+        WriteImage(info, "RADIO/", "logo.img", "/dev/block/by-name/logo")
     if 'RADIO/bootloader.img' in info.input_zip.namelist():
-        PrintInfo(info, "/dev/block/by-name/bootloader")
-        AddImage(info, "RADIO/", "bootloader.img",
-                 "/dev/block/by-name/bootloader")
+        WriteImage(info, "RADIO/", "bootloader.img",
+                   "/dev/block/by-name/bootloader")
     return
